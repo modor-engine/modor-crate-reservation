f54dfc66855f9f91072fc75889b0c7da2493cc42
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2021-12-20 16:01:31 +0100

    Fix crate detection on crates.io

--- .github/workflows/scripts/check_publication.sh
+++ .github/workflows/scripts/check_publication.sh
@@ -2,7 +2,7 @@
 set -eu
 
 while read -r crate_name; do
-    if cargo search serde --limit 1 | grep "^$crate_name = "; then
+    if cargo search "$crate_name" --limit 1 | grep "^$crate_name = "; then
         echo "'$crate_name' already published"
     else
         sed -i "s/^name \?=.*$/name = \"$crate_name\"/g" Cargo.toml
