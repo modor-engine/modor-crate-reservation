2a7cc153c0956b18806df5fd978f3e9b02d9794d
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2021-12-20 15:15:44 +0100

    Add project template to register first crates

---
+++ .github/workflows/scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+while read -r crate_name; do
+    if cargo search serde --limit 1 | grep "^$crate_name = "; then
+        echo "'$crate_name' already published"
+    else
+        sed -i "s/^name \?=.*$/name = \"$crate_name\"/g" Cargo.toml
+        cargo publish --dry-run --allow-dirty --token "$CRATES_IO_TOKEN"
+    fi
+done < crate_names
+
+sed -i "s/^name \?=.*$/name = \"\"/g" Cargo.toml
